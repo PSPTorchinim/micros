d494f0b8327b2faacc61cb4f6518891f61c33fed
Hubert Troć <huberttroc@gmail.com>
2025-10-26 11:47:58 +0100

    Connected frontend to Strapi CMS and created dynamic content loader

---
+++ Frontends/dj-panel/src/services/strapiPages.ts
@@ -0,0 +1,50 @@
+import { Api, Page } from '../models/strapi/strapiMap';
+
+class StrapiAPI {
+  private api: Api<unknown>;
+
+  constructor() {
+    const protocol = process.env.REACT_APP_CMS_DATABASE_PROTOCOL || 'http';
+    const host = process.env.REACT_APP_CMS_DATABASE_HOST || 'localhost';
+    const port = process.env.REACT_APP_CMS_DATABASE_PORT || '1337';
+    const apiPath = process.env.REACT_APP_CMS_DATABASE_API_PATH || '/api';
+    const baseURL = `${protocol}://${host}:${port}${apiPath}`;
+    this.api = new Api({ baseURL });
+  }
+
+  async fetchRootPages(): Promise<Page[]> {
+    // Fetch pages that are published and do not have Parents (capital P, plural)
+    const response = await this.api.page.getPages({
+      filters: {
+        publishedAt: { $notNull: true },
+        Parents: { id: { $null: true } },
+      },
+    });
+    return response.data?.data || [];
+  }
+
+  async fetchPageById(id: number | string): Promise<Page | undefined> {
+    const response = await this.api.page.getPagesId(Number(id));
+    return response.data?.data;
+  }
+
+  async fetchPageByName(name: string): Promise<Page | undefined> {
+    const response = await this.api.page.getPages({
+      filters: { Title: { $eq: name } },
+    });
+    return response.data?.data?.[0];
+  }
+
+  async fetchPagesByParentId(parentId: number | string): Promise<Page[]> {
+    // Fetch pages where Parents contains the given parentId
+    const response = await this.api.page.getPages({
+      filters: {
+        Parents: { id: { $eq: parentId } },
+        publishedAt: { $notNull: true },
+      },
+    });
+    return response.data?.data || [];
+  }
+}
+
+export const strapiAPI = new StrapiAPI();
