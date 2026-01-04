c2f111967ce13bca12a823a7950e87b82f2d5b89
Hubert Troć <huberttroc@gmail.com>
2026-01-04 01:27:43 +0100

    revert "as any"

--- CMS/src/api/change-password-block/services/change-password-block.ts
+++ CMS/src/api/change-password-block/services/change-password-block.ts
@@ -1,5 +1,3 @@
 import { factories } from "@strapi/strapi";
 
-// Type assertion needed until Strapi generates types for this new content type
-// Run `strapi ts:generate-types` after deployment to generate proper types
-export default factories.createCoreService("api::change-password-block.change-password-block" as any);
+export default factories.createCoreService("api::change-password-block.change-password-block");
