77670c423e4208be663b5e64112aa178315a8f59
Hubert Troć <huberttroc@gmail.com>
2026-02-04 21:47:04 +0100

    fixup

--- Frontends/dj-panel/src/components/molecules/ForgotPasswordForm/ForgotPasswordForm.test.tsx
+++ Frontends/dj-panel/src/components/molecules/ForgotPasswordForm/ForgotPasswordForm.test.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// @ts-ignore - React is needed for JSX
+import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ForgotPasswordForm } from './ForgotPasswordForm';
