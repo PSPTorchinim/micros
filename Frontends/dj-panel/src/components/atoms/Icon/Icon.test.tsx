596eb3ede34e657483ce5db033f7b4078aa054c4
Hubert Troć <47593054+PSPTorchinim@users.noreply.github.com>
2026-02-05 07:46:02 +0100

    Add React import for Icon component tests

--- Frontends/dj-panel/src/components/atoms/Icon/Icon.test.tsx
+++ Frontends/dj-panel/src/components/atoms/Icon/Icon.test.tsx
@@ -1,4 +1,6 @@
 import '@testing-library/jest-dom';
+// @ts-ignore - React is needed for JSX
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
 import { render } from '@testing-library/react';
 import { AiFillHeart } from 'react-icons/ai';
